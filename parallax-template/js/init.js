c9fbd18a8d3e0d8abe31b415f4c0f6435a023672
Benjamin D. Horn <hornbd96@gmail.com>
2016-09-12 17:48:01 -0400

    mat google

---
+++ parallax-template/js/init.js
@@ -0,0 +1,10 @@
+(function ($) {
+  $(function(){
+
+    $('.button-collapse').sideNav();
+    $('.parallax').parallax();
+      $("#footer").load("footer.html"); 
+      $("#menunav").load("menunav.html");
+$("#menu").load("menu.html");
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
