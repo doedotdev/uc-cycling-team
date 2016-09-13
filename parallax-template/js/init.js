e84d80066f6331db4247bfa3ae0a2abd0873b10b
Benjamin D. Horn <hornbd96@gmail.com>
2016-09-13 14:26:55 -0400

    materialize the site

--- parallax-template/js/init.js
+++ parallax-template/js/init.js
@@ -8,3 +8,9 @@
 $("#menu").load("menu.html");
   }); // end of document ready
 })(jQuery); // end of jQuery name space
+
+  $(document).ready(function(){
+    $('.collapsible').collapsible({
+      accordion : false // A setting that changes the collapsible behavior to expandable instead of the default accordion style
+    });
+  });
