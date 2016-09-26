46a94736c86a7c2eec33c47cd6f6b32712e47a35
Benjamin D. Horn <hornbd96@gmail.com>
2016-09-26 16:08:28 -0400

    updated logo

--- groups/uccyclingclub/js/init.js
+++ groups/uccyclingclub/js/init.js
@@ -19,3 +19,50 @@ $("#menu").load("menu.html");
     // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
     $('.modal-trigger').leanModal();
   });
+
+google.charts.load('current', {'packages':['line', 'corechart']});
+      google.charts.setOnLoadCallback(drawChart);
+
+    function drawChart() {
+
+      var chartDiv = document.getElementById('chart_div');
+
+      var data = new google.visualization.DataTable();
+      data.addColumn('date', 'Test Date');
+      data.addColumn('number', "Max Heart Rate");
+      data.addColumn('number', "Power at LT");
+
+      data.addRows([
+        [new Date(2014, 0),  115,  170],
+        [new Date(2014, 8),   120,  185],
+        [new Date(2015, 1),   128,   195],
+        [new Date(2015, 6),  140, 210],
+        [new Date(2016, 2),  150, 225]
+      ]);
+
+      var materialOptions = {
+        chart: {
+          title: 'Power at LT + Max Heart Rate'
+        },
+        width: 1000,
+        height: 600,
+
+        axes: {
+          // Adds labels to each axis; they don't have to match the axis names.
+          y: {
+            Temps: {label: 'Power at LT'},
+            Daylight: {label: 'Max Heart Rate'}
+          }
+        }
+      };
+
+
+
+      function drawMaterialChart() {
+        var materialChart = new google.charts.Line(chartDiv);
+        materialChart.draw(data, materialOptions);
+      }
+
+      drawMaterialChart();
+
+    }
